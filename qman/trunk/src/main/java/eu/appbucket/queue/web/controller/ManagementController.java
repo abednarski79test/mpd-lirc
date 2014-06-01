23a19259c5f7cafded9ecbd5667d81660a749009
abednarski79@gmail.com <abednarski79@gmail.com@7c76d5c5-cfa3-7c1b-3f54-661a1e0552a6>
2014-06-01 19:07:41 +0000

    add new queue + add reset cache endpoint

---
+++ qman/trunk/src/main/java/eu/appbucket/queue/web/controller/ManagementController.java
@@ -0,0 +1,30 @@
+package eu.appbucket.queue.web.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import eu.appbucket.queue.core.service.ManagementService;
+
+@Controller
+public class ManagementController {
+	
+	private static final Logger LOGGER = Logger.getLogger(ManagementController.class);
+	private ManagementService managementService;
+	
+	@Autowired
+	public void setManagementService(ManagementService managementService) {
+		this.managementService = managementService;
+	}
+	
+	@RequestMapping(value = "resetCache", method = RequestMethod.GET)
+	@ResponseBody
+	public void resetCache() {
+		LOGGER.info("resetCache");
+		managementService.resetCache();
+		LOGGER.info("resetCache.");
+	}
+}
