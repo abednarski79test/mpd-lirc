b43e5e9d9b4b60b8dcfa22f99311eb1dc7fe65fd
abednarski79@gmail.com <abednarski79@gmail.com@7c76d5c5-cfa3-7c1b-3f54-661a1e0552a6>
2014-06-06 17:01:16 +0000

    geo location updated

---
+++ qman/trunk/doc/db_setup/db schema v9/updates.sql
@@ -0,0 +1,14 @@
+UPDATE queues_details set 
+latitude = 53.347449,
+longitude = -6.25658
+where queue_id in (1, 2);
+
+UPDATE queues_details set 
+latitude = 52.243368,
+longitude = 21.00163
+where queue_id = 3;
+
+UPDATE queues_details set 
+latitude = 53.350534,
+longitude = -6.266111
+where queue_id = 4;
