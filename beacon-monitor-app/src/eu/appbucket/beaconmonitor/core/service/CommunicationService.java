d77217ce9f2986f46bd985e5efdb016cf50dc182
abednarski79@gmail.com <abednarski79@gmail.com@7c76d5c5-cfa3-7c1b-3f54-661a1e0552a6>
2014-07-14 15:17:54 +0000

    Network added

--- beacon-monitor-app/src/eu/appbucket/beaconmonitor/core/service/CommunicationService.java
+++ beacon-monitor-app/src/eu/appbucket/beaconmonitor/core/service/CommunicationService.java
@@ -4,11 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import uk.co.alt236.bluetoothlelib.device.BluetoothLeDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Parcelable;
 import android.util.Log;
+import eu.appbucket.beaconmonitor.core.constants.App;
 
-public class CommunicationService {
+public class CommunicationService extends BroadcastReceiver {
 	
 	private static final String LOG_TAG = CommunicationService.class.getName();
+	private Context contex;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		this.contex = context;
+		Parcelable[] foundDevices = intent.getParcelableArrayExtra(App.BROADCAST_DEVICE_FOUND_PAYLOAD);
+		for(Parcelable device:  foundDevices) {		
+			reportStolenAsset((BluetoothLeDevice) device);
+		}
+	}
+	
+	public void reportStolenAsset(BluetoothLeDevice device) {
+		if(!isOnline()) {
+			log("Connection is not available");
+			return;
+		}		
+	}
+	
+	public boolean isOnline() {
+	    ConnectivityManager connMgr = (ConnectivityManager) contex.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+	    return (networkInfo != null && networkInfo.isConnected());
+	}  
+	
 	
 	public UUID[] getStoleAssets() {
 		List<UUID> stoleAssets = new ArrayList<UUID>();
@@ -16,9 +48,9 @@ public class CommunicationService {
 		UUID[] stolenUUIDs = new UUID[stoleAssets.size()];
 		stoleAssets.toArray(stolenUUIDs);
 		return stolenUUIDs;
-	}
+	}	
 	
-	public void reportStolenAsset(String foundAsset) {
-		Log.d(LOG_TAG, "Reporting found assets: " + foundAsset);
+	private void log(String log) {
+		Log.d(LOG_TAG, log);
 	}
 }
